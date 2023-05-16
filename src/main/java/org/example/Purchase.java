bc2a47f7eb57bc23938f7a162596fc0ee0a24f49
SvetlanaMaeva <mystik20009@gmail.com>
2023-05-16 18:15:24 +0300

    initial commit

---
+++ src/main/java/org/example/Purchase.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Map;
+
+public class Purchase {
+    String title;
+    int count;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
